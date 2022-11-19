538a3b55c7f9596b8c8f80fb635e3ac124149855
Dane Bertram <dbertram@gmail.com>
2022-11-18 22:18:32 -0500

    Star using Eleventy SSG (#1)
    * start using eleventy SSG
    
    - update "yarn build" to run eleventy
    - add "yarn start" (and alias "yarn dev") that run eleventy dev server
    - drop sass in favor of eleventy-sass
      - rename non-entrypoint scss files to start with _ to avoid being copied to output dir
    - git ignore _site (eleventy default output directory)
    
    * netlify: add netlify.toml to use new eleventy-based build output
    
    * scss: fix sass / division deprecation warning

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+const eleventySass = require('eleventy-sass')
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPlugin(eleventySass)
+
+    eleventyConfig.addPassthroughCopy("favicon.ico")
+    eleventyConfig.addPassthroughCopy("images/*")
+    eleventyConfig.addPassthroughCopy("js/*")
+}
