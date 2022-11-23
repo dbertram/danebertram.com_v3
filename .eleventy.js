11b3c18b0c4a74fa003172dc432f3b306056e18d
Dane Bertram <dbertram@gmail.com>
2022-11-22 22:06:32 -0500

    layout: use checkbox + general sibling selector for expandable content
    - removes jQuery dependency

--- .eleventy.js
+++ .eleventy.js
@@ -5,5 +5,4 @@ module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("favicon.ico")
     eleventyConfig.addPassthroughCopy("images/*")
-    eleventyConfig.addPassthroughCopy("js/*")
 }
