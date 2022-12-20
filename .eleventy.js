6e574b1bb9b513b68f0658cbab0fe2261da9e531
Dane Bertram <dbertram@gmail.com>
2022-12-20 14:55:43 -0500

    Formatting: standardize on 2-space indentation

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,8 @@
 const eleventySass = require('eleventy-sass')
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPlugin(eleventySass)
+  eleventyConfig.addPlugin(eleventySass)
 
-    eleventyConfig.addPassthroughCopy("favicon.ico")
-    eleventyConfig.addPassthroughCopy("images/*")
+  eleventyConfig.addPassthroughCopy("favicon.ico")
+  eleventyConfig.addPassthroughCopy("images/*")
 }
