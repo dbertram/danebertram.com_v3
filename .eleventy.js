49717d0e4adc3030f96b5307344bff7bdcbfde06
Dane Bertram <dbertram@gmail.com>
2026-02-28 09:44:13 -0500

    upgrade to eleventy v3 + migrate to modern sass

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,5 @@
-const eleventySass = require('eleventy-sass')
-
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(eleventySass)
-
   eleventyConfig.addPassthroughCopy("favicon.ico")
   eleventyConfig.addPassthroughCopy("images/*")
+  eleventyConfig.addPassthroughCopy("css/*.css")
 }
