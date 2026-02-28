5b2a310f30373c981a3e9286878e8e3869a51e6a
Dane Bertram <dbertram@gmail.com>
2026-02-28 10:22:30 -0500

    refactor to use ninja templates + data json files
    - uses eleventy filters and short codes to handle convenience things like formatting dates, inlining svgs, etc.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,63 @@
+const fs = require("node:fs")
+const path = require("node:path")
+
+const monthYearFormatter = new Intl.DateTimeFormat("en-US", {
+  month: "long",
+  year: "numeric",
+  timeZone: "UTC",
+})
+
+function formatMonthYear(value) {
+  if (!value) {
+    return ""
+  }
+
+  const match = /^(\d{4})-(\d{2})$/.exec(value)
+  if (!match) {
+    return value
+  }
+
+  const year = Number(match[1])
+  const month = Number(match[2])
+  if (month < 1 || month > 12) {
+    return value
+  }
+
+  return monthYearFormatter.format(new Date(Date.UTC(year, month - 1, 1)))
+}
+
 module.exports = function (eleventyConfig) {
+  const iconsDirectory = path.join(__dirname, "_includes", "icons")
+  const iconCache = new Map()
+
+  eleventyConfig.addShortcode("inlineIcon", (iconName) => {
+    if (!iconName) {
+      return ""
+    }
+
+    let iconSource = iconCache.get(iconName)
+    if (!iconSource) {
+      const iconPath = path.join(iconsDirectory, `${iconName}.svg`)
+      if (!fs.existsSync(iconPath)) {
+        throw new Error(`Unknown icon: ${iconName}`)
+      }
+
+      iconSource = fs.readFileSync(iconPath, "utf8").trim()
+      iconCache.set(iconName, iconSource)
+    }
+    return iconSource
+  })
+
+  eleventyConfig.addFilter("monthYear", (value) => formatMonthYear(value))
+
+  eleventyConfig.addFilter("monthYearOrPresent", (value) => {
+    if (!value) {
+      return "Present"
+    }
+
+    return formatMonthYear(value)
+  })
+
   eleventyConfig.addPassthroughCopy("favicon.ico")
   eleventyConfig.addPassthroughCopy("images/*")
   eleventyConfig.addPassthroughCopy("css/*.css")
